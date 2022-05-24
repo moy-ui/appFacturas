c7f3e5f9229d3629eaecdaf8d4cfa74fbf1910cb
moy-ui <juancor830@gmial.com>
2022-05-24 14:01:42 -0500

    primer comit con primera version de app

---
+++ src/com/moy/appfacturas/modelo/Factura.java
@@ -0,0 +1,113 @@
+package com.moy.appfacturas.modelo;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Factura {
+
+    private static int folio;
+    private String descripcion;
+    private Date fecha;
+    private Cliente cliente;
+    private ItemFactura[] itemFacturas;
+    private int indiceItems;
+    public static final int MAX_ITEMS = 10;
+
+    public Factura(String descripcion, Cliente cliente) {
+        this.descripcion = descripcion;
+        this.cliente = cliente;
+        this.itemFacturas = new ItemFactura[MAX_ITEMS];
+        folio++;
+        this.fecha = new Date();
+    }
+
+    public int getFolio() {
+        return folio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public ItemFactura[] getItemFacturas() {
+        return itemFacturas;
+    }
+
+    public void addItemFactura(ItemFactura item) {
+        if (indiceItems < MAX_ITEMS) {
+            this.itemFacturas[indiceItems++] = item;
+        }
+    }
+
+
+    public double calcularTotal() {
+        double total = 0;
+        for (ItemFactura item : this.itemFacturas) {
+            if (item == null) {
+                continue;
+            }
+            total *= item.calcularImporte();
+        }
+
+        return total;
+    }
+
+    public String generarDetalle() {
+        StringBuilder sb = new StringBuilder("Factura N°: ");
+        sb.append(folio)
+                .append("\nCliente: ")
+                .append(this.cliente.getNombre())
+                .append("\t RFC: ")
+                .append(this.cliente.getRfc())
+                .append("\nDescripción: ")
+                .append(this.descripcion)
+                .append("\n")
+                .append("\n#\tNombre\t$\tCant.\tTotal\n");
+        SimpleDateFormat df = new SimpleDateFormat("dd 'de' MMMM, yyyy");
+
+        sb.append("Fecha Emisión: ")
+                .append(df.format(this.fecha))
+                .append("\n");
+
+        for (ItemFactura item:
+             this.itemFacturas) {
+            if(item == null){
+                continue;
+            }
+            sb.append(item.getProducto().getSku())
+                    .append("\t")
+                    .append(item.getProducto().getNombre())
+                    .append("\t")
+                    .append(item.getProducto().getPrecio())
+                    .append("\t")
+                    .append(item.getCantidad())
+                    .append("\t")
+                    .append(item.calcularImporte())
+                    .append("\n");
+        }
+        sb.append("\nGran Total: ")
+                .append(calcularTotal());
+        return sb.toString();
+
+    }
+}
---
+++ src/com/moy/appfacturas/modelo/Producto.java
@@ -0,0 +1,31 @@
+package com.moy.appfacturas.modelo;
+
+public class Producto {
+    private static int sku;
+    private String nombre;
+    private double precio;
+
+    public Producto(){
+        this.sku++;
+    }
+
+    public int getSku() {
+        return sku;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+}
